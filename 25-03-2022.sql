5ed6726117660648b955312c1c7ae50827d8fb90
poorna99122 <poornachandrarao99122@gmail.com>
2022-03-25 15:07:35 -0700

    Default Concept is done

--- 25-03-2022.sql
+++ 25-03-2022.sql
@@ -17,3 +17,49 @@ insert  into test_notnull values('poorna','chandra','rao',null);
 select * from test_notnull;
 
 
+alter table test_notnull modify lastName varchar2(35) not null;
+commit;
+
+
+
+-----------------------------------------------------------------
+
+
+create table test_default (
+firstName varchar2(35),
+secondName varchar2(35),
+lastName varchar2(35),
+phoneNumber number(10) default '1111111111'
+);
+
+
+select * from test_default;
+
+insert  into test_default values('poorna','chandra','rao','');
+
+
+CREATE TABLE demo_tbl
+(
+salary number(8,2) DEFAULT 9500,
+hire_date DATE DEFAULT '01-JAN-2011' ,
+birthdate DATE DEFAULT SYSDATE
+);
+
+
+select * from demo_tbl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
