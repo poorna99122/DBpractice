c929d8698c6dcbad77cab4db4dbe9da3c73bed4c
poorna99122 <poornachandrarao99122@gmail.com>
2022-03-24 20:41:40 -0700

    Savepoint and RollBack

--- 23-03-2022.sql
+++ 23-03-2022.sql
@@ -16,13 +16,23 @@ language  varchar2(30)
 
 insert  into movie_new values('RRR','Action',TO_DATE('25-03-22','DD-MM-YY'),'3HRS','TELUGU');
 insert  into movie_new values('BEAST','Action',TO_DATE('26-03-22','DD-MM-YY'),'3HRS','TAMIL');
+SAVEPOINT P1;
+
 insert  into movie_new values('MI','Action',TO_DATE('26-03-22','DD-MM-YY'),'3HRS','ENGLISH');
+SAVEPOINT P2;
+
 insert  into movie_new values('SUPERMAN','Action',TO_DATE('30-03-22','DD-MM-YY'),'3HRS','ENGLISH');
+SAVEPOINT P3;
+
 
 select * from movie_new;
 
+ROLLBACK TO P1;
+ROLLBACK TO P2;
 
-DELETE FROM MOVIE_NEW WHERE MOVIE_NAME = 'SPIDERMAN';
+
+DELETE FROM MOVIE_NEW WHERE MOVIE_NAME IN ('SUPERMAN','MI','BEAST');
+COMMIT; 
 
 ROLLBACK;
 
